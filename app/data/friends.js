d1222254dfce7882dd7ec21b81b5c3a5ec3ba54d
Lance <innerlewd@gmail.com>
2019-11-19 15:04:18 -0500

    finished up friends database

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name: "Bruce Willis",
+        photo: "https://cdn.britannica.com/s:700x500/48/194248-050-4EE825CF/Bruce-Willis-2013.jpg",
+        scores: [5, 1, 1, 2, 5, 4, 5, 4, 2, 2]
+    }, 
+    {
+        name: "Cameron Monaghan",
+        photo: "https://i.ytimg.com/vi/0YMugeAVRyg/maxresdefault.jpg",
+        scores: [1, 1, 2, 1, 1, 1, 1, 1, 1, 1],
+    },
+    {
+        name: "Harry Kane",
+        photo: "https://talksport.com/wp-content/uploads/sites/5/2019/11/NINTCHDBPICT000540682605-e1574018120137.jpg?strip=all&w=960&quality=100",
+        scores: [2, 4, 5, 1, 2, 2, 5, 1, 3, 4],
+    },
+    {
+        name: "Joseph Stalin",
+        photo: "https://www.biography.com/.image/t_share/MTY2NjgyOTkyNTMyNTMwMjMx/gettyimages-2637237.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    },
+    {
+        name: "Aubrey Graham",
+        photo: "https://thenypost.files.wordpress.com/2019/11/drake-boo-flog-gnaw-01.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    },
+    {
+        name: "Action Bronson",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/10/31/13/action-bronson.jpg?w968h681",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    },
+    {
+        name: "POORSTACY",
+        photo: "https://i1.sndcdn.com/artworks-000552164946-iplm3z-t500x500.jpg",
+        scores: [2, 2, 4, 2, 4, 2, 1, 2, 3, 2],
+    },
+    {
+        name: "Iann Dior",
+        photo: "https://images.genius.com/0426d4d4cede6924fa3cb01326fea596.998x1000x1.jpg",
+        scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+    },
+    {
+        name: "Trippie Redd",
+        photo: "https://www.sohh.com/wp-content/uploads/2018/06/Trippie-Redd-759x500.jpg",
+        scores: [3, 4, 2, 3, 1, 2, 3, 5, 4, 5],
+    },
+    {
+        name: "Doja Cat",
+        photo: "https://images.genius.com/5ef0264b344f1861dd4002f77b598227.1920x1080x1.png",
+        scores: [4, 4, 4, 4, 4, 4, 4, 1, 5, 4],
+    },
+];
+
+module.exports = friends;
